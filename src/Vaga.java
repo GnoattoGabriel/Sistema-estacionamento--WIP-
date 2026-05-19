f19ddb29546db955722ff5da87bbaf9922810061
Maria <dudahpereira13@gmail.com>
2026-05-19 12:52:06 -0300

    questao 5

--- src/Vaga.java
+++ src/Vaga.java
@@ -21,6 +21,22 @@ public class Vaga {
         this.horarioReserva = horarioReserva;
     }
 
+    public String descreverStatus(Vaga vaga) {
+        if (vaga.isOcupada() && vaga.isCoberta()) {
+            return "ocupada e coberta";
+        }
+
+        if (vaga.isOcupada()) {
+            return "ocupada";
+        }
+
+        if (vaga.isCoberta()) {
+            return "livre e coberta";
+        }
+
+        return "livre";
+    }
+
     public String getCodigo() {
         return codigo;
     }
